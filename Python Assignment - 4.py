bb72e4c3d63bdf0b7994e8c57f823aaef1efa0e1
kurkurevaibhav <64495374+kurkurevaibhav@users.noreply.github.com>
2020-05-22 17:21:16 +0530

    Add files via upload

---
+++ Python Assignment - 4.py
@@ -0,0 +1,88 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+Assignment - 4 (Lists)
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+Q check if a given number is prime or not? 	
+
+#-----------------------------------------------------------------
+
+Q Find the position of an number in a unsorted array.
+
+#-----------------------------------------------------------------
+
+Q Find the position of an number in a sorted array.
+
+#-----------------------------------------------------------------
+
+Q Find the Nth Fibonacci number
+Example
+Input: 15
+Output: 610 
+
+#-----------------------------------------------------------------
+
+Q Find the Factorial of a number
+
+Example
+Input: 6
+Output: 720 
+
+#-----------------------------------------------------------------
+
+
+ASSIGMENTS ON OPPS 
+
+
+
+
+Level 1
+Answer the following questions.
+
+What is a class?
+What is an instance?
+What is inheritance?
+What is multiple inheritance?
+What is polymorphism?
+What is method overriding and overloading ?
+How to you delete the object ? Explain 
+
+
+#------------------------------------------------------------------------------------------------------------------------
+
+Level 2 
+
+1.Create a Cricle class and intialize it with radius. Make two methods getArea and getCircumference inside this class.
+
+
+2. Create a Temprature class. Make two methods :
+1.convertFahrenheit - It will take celsius and will print it into Fahrenheit.
+2 convertCelsius - It will take Fahrenheit and will convert it into Celsius.
+
+
+3. Create a Student class and initialize it with name and roll number. Make methods to :
+1. Display - It should display all informations of the student.
+2. setAge - It should assign age to student
+3. setMarks - It should assign marks to the student.
+
+
+4.Create a Time class and initialize it with hours and minutes.
+1. Make a method addTime which should take two time object and add them. E.g.- (2 hour and 50 min)+(1 hr and 20 min) is (4 hr and 10 min)
+2. Make a method displayTime which should print the time.
+3. Make a method DisplayMinute which should display the total minutes in the Time. E.g.- (1 hr 2 min) should display 62 minute.
+
+#------------------------------------------------------------------------------------------------------------------------
+
+Level 3 
+
+
+
+Level 3 
+Create a deck of cards class. Internally, the deck of cards should use another class, a card class. Your requirements are:
+
+The Deck class should have a deal method to deal a single card from the deck
+After a card is dealt, it is removed from the deck.
+There should be a shuffle method which makes sure the deck of cards has all 52 cards and then rearranges them randomly.
+The Card class should have a suit (Hearts, Diamonds, Clubs, Spades) and a value (A,2,3,4,5,6,7,8,9,10,J,Q,K)
+
+
+
